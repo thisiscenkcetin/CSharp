9266ca567145406ea270502496f7fb4d4085b31f
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-28 14:11:19 +0300

    Dairenin Alanını ve Çevresini Bulma(Pi) - C#

---
+++ Dairenin Alanını ve Çevresini Bulma(Pi) - C#/Dairenin Alanını ve Çevresini Bulma(Pi) - C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            // Alan: π * r * r
+            // Cevre: 2 * π * r
+
+            Console.Write("Yarıçap: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double pi = Math.PI;
+            double alan = pi * r * r;
+            double cevre = 2 * pi * r;
+
+            Console.WriteLine("Dairenin Alanı: " + alan);
+            Console.WriteLine("Dairenin Çevresi: " + cevre);
+        }
+    }
+}
