025c9ded13fbf4827aed8ad43c9fe0b8fd4bc1a5
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-28 13:24:15 +0300

    KDV hesaplama aracı – C#

---
+++ KDV hesaplama aracı – C#/KDV hesaplama aracı – C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelLoworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //100,20,120TL
+            Console.Write("Ürünün Fıyatı: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            //Kdv orani
+            double kdvRate = 0.20;
+
+            //Kdv tutari
+            double kdvPrice = price * kdvRate;
+            double totalPrice = price + kdvPrice;
+
+            Console.WriteLine("KDV Tutarı : " + kdvPrice);
+            Console.WriteLine("KDV'li Tutar : " + totalPrice);
+            //Total Price seklinde sur kucuk olacak
+        }
+    }
+}
