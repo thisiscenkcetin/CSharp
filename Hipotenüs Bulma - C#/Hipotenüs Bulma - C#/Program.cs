057e865b37867fce8ab5442e93b2db0da4f93c7b
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-28 16:39:39 +0300

    Hipotenüs Bulma - C#

---
+++ Hipotenüs Bulma - C#/Hipotenüs Bulma - C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        // √(a² + b²)
+        Console.Write("1. Kenar: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("2. Kendar: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double result = Math.Sqrt(a * a + b * b);
+
+        Console.WriteLine("Hipotenüs: " + result);
+
+
+    }
+}
