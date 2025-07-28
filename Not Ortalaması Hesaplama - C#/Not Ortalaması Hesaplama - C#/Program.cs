ac2e5cf819c19d84636ec08f4ce6d9adb61c468c
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-28 13:28:28 +0300

    Not Ortalaması Hesaplama - C#

---
+++ Not Ortalaması Hesaplama - C#/Not Ortalaması Hesaplama - C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Matematik Notunuz : ");
+            double math = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Fizik Notunuz : ");
+            double phy = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Türkçe Notunuz : ");
+            double turkish = Convert.ToDouble(Console.ReadLine());
+
+            double result = math + phy + turkish;
+            result /= 3; 
+
+            Console.WriteLine("Ortalamanız: " + result);
+        }
+    }
+}
