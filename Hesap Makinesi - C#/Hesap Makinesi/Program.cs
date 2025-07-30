772a12adb686de4ccaffba4fd295a30d71e15389
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-30 12:58:27 +0300

    Hesap Makinesi - C#

---
+++ Hesap Makinesi - C#/Hesap Makinesi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.Write("1. Sayı: ");
+        double sayi1 = double.Parse(Console.ReadLine());
+
+        Console.Write("2. Sayı: ");
+        double number2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Yapmak istediğiniz işlemi seçin (+, -, *, /): ");
+        char operation = char.Parse(Console.ReadLine());
+
+        try
+        {
+            double result = operation switch
+            {
+                '+' => sayi1 + number2,
+                '-' => sayi1 - number2,
+                '*' => sayi1 * number2,
+                '/' => number2 != 0 ? sayi1 / number2
+                                    : throw new DivideByZeroException("Bir sayı sıfıra bölünemez."),
+                _ => throw new InvalidOperationException("Geçersiz işlem.")
+            };
+
+            Console.WriteLine($"Sonuç: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+    }
+}
