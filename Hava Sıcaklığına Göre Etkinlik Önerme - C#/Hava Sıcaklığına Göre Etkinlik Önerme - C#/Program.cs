36983f57175f2406097c37444bf56c43d55b783b
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-30 12:53:48 +0300

    Hava Sıcaklığına Göre Etkinlik Önerme - C#

---
+++ Hava Sıcaklığına Göre Etkinlik Önerme - C#/Hava Sıcaklığına Göre Etkinlik Önerme - C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class ActivitySuggestion
+{
+    static void Main()
+    {
+        Console.Write("Sıcaklık Giriniz(°C): ");
+        int temperature = int.Parse(Console.ReadLine());
+
+        if (temperature < 5)
+        {
+
+            Console.WriteLine("Önerilen Etkinlik: Kayak");
+
+        }
+
+        else if (temperature >= 5 && temperature <= 15)
+        {
+            Console.WriteLine("Önerilen Etkinlik: Sinema");
+        }
+        else if (temperature > 15 && temperature <= 25)
+        {
+            Console.WriteLine("Önerilen Etkinlik: Piknik");
+        }
+        else
+        {
+            Console.WriteLine("Önerilen Etkinlik: Yüzme");
+        }
+
+    }
+}
