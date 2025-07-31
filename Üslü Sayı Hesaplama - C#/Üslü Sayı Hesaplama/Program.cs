758696d40211e1dec8e9dbb6f3500ad16d79cb9b
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-31 17:43:47 +0300

    Üslü Sayı Hesaplama - C#

---
+++ Üslü Sayı Hesaplama - C#/Üslü Sayı Hesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Kullanıcıdan üs ve üssü değerlerini alır
+            Console.Write("Üssü alınacak sayıyı girin: ");
+            double baseNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Üs değerini girin: ");
+            double exponent = double.Parse(Console.ReadLine());
+
+            double result = Math.Pow(baseNumber, exponent);
+
+            //Üssün hesaplanması
+            for (int i = 1; i <= exponent; i++)
+            {
+            result *= baseNumber;
+            }
+
+            // Sonucu ekrana yazdırır
+            Console.WriteLine($"{baseNumber} üssü {exponent} = {result}");
+
+        }
+    }
+}
