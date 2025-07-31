8be10b23d1516ef0ae2a8a30f00088dd2a32bd45
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-31 17:42:44 +0300

    Tek Sayıların Toplamını Bulan Program - C#

---
+++ Tek Sayıların Toplamını Bulan Program - C#/Tek Sayıların Toplamını Bulan Program - C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int total = 0;
+        int userInput;
+
+        Console.WriteLine("Negatif bir değer girene kadar sayıları giriniz.");
+
+        // Negatif sayı girilene kadar döngü devam eder
+        do
+        {
+            Console.Write("Bir sayı giriniz: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Sayı tek ise toplamaya ekle
+            if (userInput % 2 != 0)
+            {
+                total += userInput;
+            }
+
+        } while (userInput >= 0);
+
+        Console.WriteLine("Girilen tek sayıların toplamı: " + total);
+    }
+}
