21edc6bf9462348fd72fc0cc5d64c0ef750d51dd
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-31 17:19:15 +0300

    Faktöriyel Hesaplama - C#

---
+++ Faktöriyel Hesaplama - C#/Faktöriyel Hesaplama - C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Kullanıcıdan bir sayı alınır
+        Console.Write("Bir sayı giriniz:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //Faktöriyel hesaplama işlemi
+        long factorial = 1;
+        for (int i = 1; i <= number; i++)
+
+        { 
+            factorial *= i; 
+        }
+
+        {
+            //Sonucu ekrana yazdırma
+            Console.WriteLine($"{number} sayısının faktöriyeli: {factorial}");
+        }
+
+    }
+}
