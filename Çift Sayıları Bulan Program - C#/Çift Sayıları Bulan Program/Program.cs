3fdc75671b94a0eed2ec7a807f073d71fdc2ed44
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-31 17:05:35 +0300

    Çift Sayıları Bulan Program - C#

---
+++ Çift Sayıları Bulan Program - C#/Çift Sayıları Bulan Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{ static void Main()
+
+    {
+
+        Console.Write("Bir sayı giriniz: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Girilsen sayıya kadar olan çift sayılar:");
+        for (int i = 2; i <= userNumber; i += 2)
+
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+
+}
