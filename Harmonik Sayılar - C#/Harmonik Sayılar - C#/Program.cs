b6f03e63ab9ab21a02df9bd771777814783b9a19
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-31 17:37:29 +0300

    Harmonik Sayılar - C#

---
+++ Harmonik Sayılar - C#/Harmonik Sayılar - C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main() // 'class' yerine 'static' olmalı
+    {
+        // Kullanıcıdan bir sayı alınır
+        Console.Write("Bir sayı giriniz: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Harmonik sayı hesaplanır
+        double harmonicSum = 0.0;
+        for (int i = 1; i <= number; i++)
+        {
+            harmonicSum += 1.0 / i;
+        }
+
+        // Sonuç ekrana yazdır
+        Console.WriteLine($"{number} sayısının harmonik serisi: {harmonicSum}");
+    }
+}
