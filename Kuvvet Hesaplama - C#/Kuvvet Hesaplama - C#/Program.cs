0823a346cffdd6e413889639e3648543e964eb5e
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-07-31 17:38:20 +0300

    Kuvvet Hesaplama - C#

---
+++ Kuvvet Hesaplama - C#/Kuvvet Hesaplama - C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    { 
+        //Kullanıcıdan bir sayı alınır
+        Console.Write("Bir sayı girin:" );
+        int number = int.Parse(Console.ReadLine());
+
+        //3'ün kuvvetlerini hesaplayan for döngüsü
+        for (int powerOfThree = 1; powerOfThree <= number; powerOfThree *= 3)
+
+        {
+            Console.WriteLine(powerOfThree);
+        }
+
+    }
+}
